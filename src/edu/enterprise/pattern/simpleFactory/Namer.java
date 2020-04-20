8baced7951415cdb29edc8138139e2ef783dcd72
Nick <nick.lebedev96@gmail.com>
2020-04-21 01:20:55 +0200

    add simple factory pattern impl

---
+++ src/edu/enterprise/pattern/simpleFactory/Namer.java
@@ -0,0 +1,14 @@
+package edu.enterprise.pattern.simpleFactory;
+
+public abstract class Namer {
+    String frName;
+    String lName;
+
+    public String getFrname() {
+        return frName;
+    }
+
+    public String getLname() {
+        return lName;
+    }
+}
